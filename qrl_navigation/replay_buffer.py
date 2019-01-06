cf3336b66f6919fccc1b66b9628608f72775d74f
Adam Kolar <adakolar@gmail.com>
2019-01-06 15:26:32 +0100

    feat: Add basic replay buffer

---
+++ qrl_navigation/replay_buffer.py
@@ -0,0 +1,49 @@
+from collections import namedtuple, deque
+import numpy as np
+import random
+import torch
+
+
+class ReplayBuffer:
+    """Fixed-size buffer to store experience tuples."""
+    def __init__(self, action_size, buffer_size=int(1e5), batch_size=64, seed=0, device='cpu'):
+        """Initialize a ReplayBuffer object.
+
+        Params:
+            action_size (int): dimension of each action
+            buffer_size (int): maximum size of buffer
+            batch_size (int): size of each training batch
+            seed (int): random seed
+            device (str): device where tensors are proecssed
+        """
+
+        self.action_size = action_size
+        self.memory = deque(maxlen=buffer_size)
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+        self.device = device
+        self.seed = random.seed(seed)
+
+    def add(self, state, action, reward, next_state, done):
+        """Add a new experience to memory."""
+        e = self.experience(state, action, reward, next_state, done)
+        self.memory.append(e)
+
+    def sample(self):
+        """Randomly sample a batch of experiences from memory."""
+        experiences = random.sample(self.memory, k=self.batch_size)
+
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(self.device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(self.device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(self.device)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(self.device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(self.device)
+
+        return (states, actions, rewards, next_states, dones)
+
+    def is_ready_to_sample(self):
+        return len(self) > self.batch_size
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.memory)
