744cfebdf985a4caad95e9ff0a4e3c4eab53e0bb
Adam Kolar <adakolar@gmail.com>
2019-01-18 00:46:10 +0100

    feat: Add meta usega

--- qrl_navigation/training.py
+++ qrl_navigation/training.py
@@ -1,5 +1,7 @@
 from collections import deque
 import numpy as np
+import os
+import pickle
 
 
 def dqn_training(agent, env, save_path, n_episodes=2000, n_max_steps_per_episode=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995, logger=None):
@@ -16,7 +18,14 @@ def dqn_training(agent, env, save_path, n_episodes=2000, n_max_steps_per_episode
     if logger is not None:
         printer = logger.info
 
-    scores = []                        # list containing scores from each episode
+    meta = {
+        'scores': [],
+        'eps': [],
+        'meta_path': os.path.join(save_path, 'meta.pickle'),
+        'model_path': os.path.join(save_path, 'model.pt'),
+        'agent': repr(agent)
+    }
+
     scores_window = deque(maxlen=100)  # last 100 scores
     eps = eps_start                    # initialize epsilon
     best_mean_score = 0
@@ -37,14 +46,16 @@ def dqn_training(agent, env, save_path, n_episodes=2000, n_max_steps_per_episode
         printer(f'Current eps is {eps}')
 
         scores_window.append(score)
-        scores.append(score)
+        meta['scores'].append(score)
+        meta['eps'].append(eps)
         mean_score = np.mean(scores_window)
 
         printer(f'\rEpisode {i_episode}\tAverage Score: {mean_score}')
         if mean_score >= best_mean_score:
             best_mean_score = mean_score
-            agent.save(save_path)
+            agent.save(meta['model_path'])
 
+    with open(meta['meta_path'], 'wb') as wb:
+        pickle.dump(meta, wb)
     printer(f'\rWe have achieved {best_mean_score} mean score (13+ is good result)')
-    # TODO: Return dictionary with verbose info
-    return scores
+    return meta
